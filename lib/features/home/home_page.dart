f8b8a697d53de87da7dfd28619215dd1880b5a2f
salah <salahswidan212@gmail.com>
2024-12-03 14:11:40 +0200

    start in custom bottom nav bar

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace/core/resourses/constants.dart';
 import 'package:mini_nft_marketplace/core/resourses/font_managers.dart';
@@ -9,20 +10,79 @@ import 'widget/custom_card_top_seller.dart';
 import 'widget/custom_list_view_top.dart';
 import 'widget/custom_sub_title.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: Stack(
+        alignment: Alignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(60),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                height: 90,
+                color: ColorManagers.kPrimaryWhite.withOpacity(0.1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.home,
+                      color: ColorManagers.kPrimaryWhite,
+                      size: 39,
+                    ),
+                    Icon(
+                      Icons.stacked_bar_chart_rounded,
+                      color: ColorManagers.kPrimaryWhite,
+                      size: 39,
+                    ),
+                    Container(
+                      width: 39,
+                    ),
+                    Icon(
+                      Icons.search,
+                      color: ColorManagers.kPrimaryWhite,
+                      size: 39,
+                    ),
+                    Icon(
+                      Icons.person,
+                      color: ColorManagers.kPrimaryWhite,
+                      size: 39,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 20,
+            color: Colors.red,
+          )
+        ],
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           "NFT MarketPlace",
           style: TextStyle(
-              color: ColorManagers.kPrimaryWhite,
-              fontSize: 25,
-              fontFamily: FontManagers.KMain),
+            color: ColorManagers.kPrimaryWhite,
+            fontSize: 25,
+            fontFamily: FontManagers.KMain,
+          ),
         ),
         backgroundColor: ColorManagers.kPrimary,
       ),
@@ -33,15 +93,9 @@ class HomePage extends StatelessWidget {
           shrinkWrap: true,
           children: [
             customListViewTop(),
-            SizedBox(
-              height: 27,
-            ),
-            customSubTitle(
-              title: "Treaning Collections",
-            ),
-            SizedBox(
-              height: 7,
-            ),
+            const SizedBox(height: 27),
+            customSubTitle(title: "Treaning Collections"),
+            const SizedBox(height: 7),
             SizedBox(
               height: 197,
               child: ListView.separated(
@@ -49,21 +103,13 @@ class HomePage extends StatelessWidget {
                 itemBuilder: (context, index) => CustomCardCollection(
                   collectionModel: Constants.collectionList[index],
                 ),
-                separatorBuilder: (context, index) => SizedBox(
-                  width: 28,
-                ),
-                itemCount: 3,
+                separatorBuilder: (context, index) => const SizedBox(width: 28),
+                itemCount: Constants.collectionList.length,
               ),
             ),
-            SizedBox(
-              height: 27,
-            ),
-            customSubTitle(
-              title: "Top Seller",
-            ),
-            SizedBox(
-              height: 7,
-            ),
+            const SizedBox(height: 27),
+            customSubTitle(title: "Top Seller"),
+            const SizedBox(height: 7),
             SizedBox(
               height: 243,
               child: ListView.separated(
@@ -71,10 +117,8 @@ class HomePage extends StatelessWidget {
                 itemBuilder: (context, index) => CustomCardTopSeller(
                   topSellerModel: Constants.topSellerList[index],
                 ),
-                separatorBuilder: (context, index) => SizedBox(
-                  width: 28,
-                ),
-                itemCount: 3,
+                separatorBuilder: (context, index) => const SizedBox(width: 28),
+                itemCount: Constants.topSellerList.length,
               ),
             ),
           ],
