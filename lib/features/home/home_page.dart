71827106b7d718efa525146d75e0652e5fb45bea
salah <salahswidan212@gmail.com>
2024-12-03 14:29:50 +0200

    finish custom bottom nav bar with using shapborder.palygan

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace/core/resourses/constants.dart';
 import 'package:mini_nft_marketplace/core/resourses/font_managers.dart';
@@ -24,55 +25,69 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      bottomNavigationBar: Stack(
-        alignment: Alignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(60),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-              child: Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                height: 90,
-                color: ColorManagers.kPrimaryWhite.withOpacity(0.1),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      color: ColorManagers.kPrimaryWhite,
-                      size: 39,
-                    ),
-                    Icon(
-                      Icons.stacked_bar_chart_rounded,
-                      color: ColorManagers.kPrimaryWhite,
-                      size: 39,
-                    ),
-                    Container(
-                      width: 39,
-                    ),
-                    Icon(
-                      Icons.search,
-                      color: ColorManagers.kPrimaryWhite,
-                      size: 39,
-                    ),
-                    Icon(
-                      Icons.person,
-                      color: ColorManagers.kPrimaryWhite,
-                      size: 39,
-                    ),
-                  ],
+      bottomNavigationBar: Container(
+        height: 122,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60), topRight: Radius.circular(60)),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  height: 90,
+                  color: ColorManagers.kPrimaryWhite.withOpacity(0.1),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.home,
+                        color: ColorManagers.kPrimaryWhite,
+                        size: 39,
+                      ),
+                      Icon(
+                        Icons.stacked_bar_chart_rounded,
+                        color: ColorManagers.kPrimaryWhite,
+                        size: 39,
+                      ),
+                      Container(
+                        width: 39,
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: ColorManagers.kPrimaryWhite,
+                        size: 39,
+                      ),
+                      Icon(
+                        Icons.person,
+                        color: ColorManagers.kPrimaryWhite,
+                        size: 39,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: 50,
-            width: 20,
-            color: Colors.red,
-          )
-        ],
+            Positioned(
+              top: 0,
+              child: Container(
+                child: Icon(
+                  CupertinoIcons.plus,
+                  color: Colors.white,
+                ),
+                decoration: ShapeDecoration(
+                  shape: StarBorder.polygon(sides: 6, pointRounding: .5),
+                  color: Colors.white.withOpacity(0.5),
+                ),
+                height: 70,
+                width: 70,
+              ),
+            )
+          ],
+        ),
       ),
       appBar: AppBar(
         centerTitle: true,
